3a7190d0825fe2c051b13f88948371c9885e48e0
Moss <2103702@talnet.nl>
2022-09-06 17:02:21 +0200

    excercise pagina

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" style="font-family: 'IBM Plex Mono', 'Menlo','DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier', 'monospace';">
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">
+</head>
+
+<body>
+
+    <nav id="menu" role="navigation">
+        <img class="menu-image" src="https://ams.jarvis.bit-academy.nl/assets/images/logo_white.svg">
+        <img class="menu-image" src="img/main-nav.png">
+        <img class="menu-image" src="img/profile-nav.png">
+    </nav>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'header.php';
+?>
+<link rel="stylesheet" href="css/excercise.css">
+
+<main>
+    <div class="top">
+        <div class="top-left">
+            <h1>
+                <-- Excercise name</h1>
+        </div>
+        <div class="top-right">
+            <button class="need-help">I need help</button>
+            <button class="inleveren">Inleveren</button>
+
+        </div>
+
+    </div>
+    <div class="excercise-info">
+        <!-- links van de excercise met algemene info -->
+    </div>
+    <div class="exercice">
+        <!-- de excercise -->
+    </div>
+</main>
+<?php
+require 'footer.php';
+?>
